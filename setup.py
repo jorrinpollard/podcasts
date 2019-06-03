8ffdbf25c52e263dd74e511a48dfe0c7764d2958
Jorrin Pollard <hi@jorr.in>
2019-06-03 01:32:31 -0700

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import podcasts
 
 setup(
     name="podcasts",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     author="Jorrin Pollard",
     author_email="hi@jorr.in",
